e37a4e2d8d3e5db8c2d8d671639cd5fa703697ce
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-09-02 20:53:49 -0400

    Updated docstrings and xml example for the DescribeCoverage funcitonality

--- data_cube_wcs/views.py
+++ data_cube_wcs/views.py
@@ -32,10 +32,28 @@ class GetCapabilities(View):
 
 class DescribeCoverage(View):
     """Implements the DescribeCoverage funcitonality as defined by the OGC WCS 1.0 specification
+
+    Once a client has obtained summary descriptions of the coverages available from a par-ticular WCS server,
+    it may be able to make simple GetCoverage requests immediately. But in most cases the client will need
+    to issue a DescribeCoverage request to obtain a full description of one or more coverages available. The
+    server responds to such a request with an XML document describing one or more coverages served by the WCS.
+
+    A finite number of url encoded parameters are used to produce a coverage description.
+
     """
 
     def get(self, request):
-        """
+        """Handles the GET parameters for the DescribeCoverage call, returning a formatted coverage description document
+
+        GET data:
+            REQUEST: request type - fixed to "DescribeCoverage"
+            VERSION (optional): version number of the WCS server - fixed to 1.0.0
+            SERVICE: service type - fixed to "WCS"
+            COVERAGE (optional): comma seperated list of coverages to described - identified using the name values from
+                the capabilities document.
+
+        Returns:
+            Validated coverage description document
 
         """
 
