49b8eb9f647695c2d1c689b11d495afdd48deab5
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-09-07 10:41:45 -0400

    Updated the get capabilities tests to be one test per def with new docstrings

--- test/test_get_capabilities.py
+++ test/test_get_capabilities.py
@@ -7,21 +7,69 @@ from .test_wcs_spec import TestWCSSpecification
 
 
 class TestGetCapabilities(TestWCSSpecification):
-    """"""
+    """Responsible for testing the GetCapabilities functionality of the WCS server
 
-    def test_get_capabilities_request(self):
-        """Test the GetCapabilities functionality of the WCS server - Section 7.2
-
+    Reference:
         https://cite.opengeospatial.org/teamengine/about/wcs/1.0.0/site/testreq.html#7-GetCapabilities%20Operation
 
-        """
+    """
 
-        # regarding 721 - our server has a static updatesequence set, so there is no way to test whether or not 721 will pass.
+    def test_no_server_updatesequence(self):
+        """
+        The server does not advertise an UpdateSequence number.
+        When a GetCapabilities request is made with an UPDATESEQUENCE parameter, the UPDATESEQUENCE parameter is ignored.
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+            UPDATESEQUENCE = ignored
+        Results:
+            Valid XML where /WC_Capabilities exists
+
+        """
 
         params_72 = {'VERSION': "1.0.0", 'SERVICE': "WCS", 'REQUEST': "GetCapabilities", "UPDATESEQUENCE": "0"}
         response = self.query_server(params_72)
         soup = BeautifulSoup(response.text, 'xml')
         self.assertTrue(soup.find('ServiceException').attrs['code'] == "CurrentUpdateSequence")
+
+    def test_current_updatesequence(self):
+        """
+        The server advertises an UpdateSequence number
+        When a GetCapabilities request is made with an UPDATESEQUENCE parameter set to the current update sequence value,
+        then the server returns a valid exception (cose = CurrentUpdateSequence).
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+            UPDATESEQUENCE = [[VAR_CURRENT_UPDATESEQUENCE]]
+        Results:
+         	Valid XML where /ServiceExceptionReport/ServiceException[@code="CurrentUpdateSequence"] exists
+
+        """
+
+        params_72 = {'VERSION': "1.0.0", 'SERVICE': "WCS", 'REQUEST': "GetCapabilities", "UPDATESEQUENCE": "0"}
+        response = self.query_server(params_72)
+        soup = BeautifulSoup(response.text, 'xml')
+        self.assertTrue(soup.find('ServiceException').attrs['code'] == "CurrentUpdateSequence")
+
+    def test_lower_updatesequence(self):
+        """
+        The server advertises an UpdateSequence number
+        When a GetCapabilities request is made with an UPDATESEQUENCE parameter set to a value lower than the current
+        update sequence value, then the server returns capabilities XML.
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+            UPDATESEQUENCE = [[VAR_LOW_UPDATESEQUENCE]]
+        Results:
+         	Valid XML where /WCS_Capabilities exists
+
+        """
 
         params_72 = {
             'VERSION': "1.0.0",
@@ -33,6 +81,22 @@ class TestGetCapabilities(TestWCSSpecification):
         soup = BeautifulSoup(response.text, 'xml')
         self.assertTrue(soup.find('WCS_Capabilities'))
 
+    def test_higher_updatesequence(self):
+        """
+        The server advertises an UpdateSequence number
+        When a GetCapabilities request is made with an UPDATESEQUENCE parameter set to a value higher than the current
+        update sequence value, then the server returns a service exception (code="InvalidUpdateSequence ")
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+            UPDATESEQUENCE = [[VAR_HIGH_UPDATESEQUENCE]]
+        Results:
+         	Valid XML where /ServiceExceptionReport/ServiceException[@code="InvalidUpdateSequence "] exists
+
+        """
+
         params_72 = {
             'VERSION': "1.0.0",
             'SERVICE': "WCS",
@@ -43,7 +107,42 @@ class TestGetCapabilities(TestWCSSpecification):
         soup = BeautifulSoup(response.text, 'xml')
         self.assertTrue(soup.find('ServiceException').attrs['code'] == "InvalidUpdateSequence")
 
-        # regarding 735 - this is already tested above in previous api calls where no update sequence is provided
+    def test_no_request_updatesequence(self):
+        """
+        The server advertises an UpdateSequence number
+        When a GetCapabilities request is made without an UPDATESEQUENCE parameter set, then the server returns most
+        recent capabilities XML.
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+        Results:
+         	Valid XML where /WCS_Capabilities exists
+
+        """
+
+        params_72 = {
+            'VERSION': "1.0.0",
+            'SERVICE': "WCS",
+            'REQUEST': "GetCapabilities",
+        }
+        response = self.query_server(params_72)
+        soup = BeautifulSoup(response.text, 'xml')
+        self.assertTrue(soup.find('WCS_Capabilities'))
+
+    def test_no_section_parameter(self):
+        """
+        When a GetCapabilities request is made without a SECTION parameter, then the entire capabilities are returned.
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+        Results:
+            Valid XML where /WCS_Capabilities/Service, /WCS_Capabilities/Capability, and /WCS_Capabilities/ContentMetadata exist.
+
+        """
 
         params_72 = {'VERSION': "1.0.0", 'SERVICE': "WCS", 'REQUEST': "GetCapabilities"}
         response = self.query_server(params_72)
@@ -52,12 +151,34 @@ class TestGetCapabilities(TestWCSSpecification):
             soup.find('WCS_Capabilities') and soup.find('Capability') and soup.find('ContentMetadata') and
             soup.find('Service'))
 
+    def test_slash_section_parameter(self):
+        """
+        When a GetCapabilities request is made with a SECTION parameter set to a value as "/", then the entire
+        capabilities are returned.
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+            SECTION = /
+        Results:
+            Valid XML where /WCS_Capabilities/Service, /WCS_Capabilities/Capability, and /WCS_Capabilities/ContentMetadata exist
+
+        """
+
         params_72 = {'VERSION': "1.0.0", 'SERVICE': "WCS", 'REQUEST': "GetCapabilities", "SECTION": "/"}
         response = self.query_server(params_72)
         soup = BeautifulSoup(response.text, 'xml')
         self.assertTrue(
             soup.find('WCS_Capabilities') and soup.find('Capability') and soup.find('ContentMetadata') and
             soup.find('Service'))
+
+    def test_section_service_parameter(self):
+        """
+        When a GetCapabilities request is made with a SECTION parameter set to a value as "/WCS_Capabilities/Service",
+        Capabilities returned includes the only portion: Service.
+
+        """
 
         params_72 = {
             'VERSION': "1.0.0",
@@ -67,7 +188,16 @@ class TestGetCapabilities(TestWCSSpecification):
         }
         response = self.query_server(params_72)
         soup = BeautifulSoup(response.text, 'xml')
-        self.assertTrue(soup.find('WCS_Capabilities') and soup.find('Service'))
+        self.assertTrue(
+            soup.find('WCS_Capabilities') and soup.find('Service') and not soup.find('Capability') and
+            not soup.find('ContentMetadata'))
+
+    def test_section_capability_parameter(self):
+        """
+        When a GetCapabilities request is made with a SECTION parameter set to a value as "/WCS_Capabilities/Capability",
+        Capabilities returned includes the only portion: Capability.
+
+        """
 
         params_72 = {
             'VERSION': "1.0.0",
@@ -77,7 +207,16 @@ class TestGetCapabilities(TestWCSSpecification):
         }
         response = self.query_server(params_72)
         soup = BeautifulSoup(response.text, 'xml')
-        self.assertTrue(soup.find('WCS_Capabilities') and soup.find('Capability'))
+        self.assertTrue(
+            soup.find('WCS_Capabilities') and soup.find('Capability') and not soup.find('ContentMetadata') and
+            not soup.find('Service'))
+
+    def test_section_content_metadata_parameter(self):
+        """
+        When a GetCapabilities request is made with a SECTION parameter set to a value as "/WCS_Capabilities/ContentMetadata",
+        Capabilities returned includes the only portion: ContentMetadata.
+
+        """
 
         params_72 = {
             'VERSION': "1.0.0",
@@ -87,16 +226,30 @@ class TestGetCapabilities(TestWCSSpecification):
         }
         response = self.query_server(params_72)
         soup = BeautifulSoup(response.text, 'xml')
-        self.assertTrue(soup.find('WCS_Capabilities') and soup.find('ContentMetadata'))
+        self.assertTrue(
+            soup.find('WCS_Capabilities') and not soup.find('Capability') and soup.find('ContentMetadata') and
+            not soup.find('Service'))
 
-    def test_get_capabilities_response(self):
-        """Test the GetCapabilities response - Section 7.3
+    def test_get_capabilities_updatesequence(self):
+        """The response to a GetCapabilities has an increased UpdateSequence value after the update service content.
 
-        https://cite.opengeospatial.org/teamengine/about/wcs/1.0.0/site/testreq.html#7.3-GetCapabilities%20Response
-
+        Untestable as per the documentation - pass
         """
 
-        # regarding 7.3.1 - not testable as per spec
+        pass
+
+    def test_get_capabilities_response(self):
+        """
+        Each OnlineResource URL intended for HTTP Get requests in the capabilities document is a URL prefix.
+
+        Request:
+            VERSION = [[VAR_WCS_VERSION]]
+            SERVICE = WCS
+            REQUEST = GetCapabilities
+        Results:
+            XML where each /WCS_Capabilities/Capability/Request/OnlineResource@xlink:href ends in a ? or both contains a ? and ends in an &.
+
+        """
 
         params_73 = {'VERSION': "1.0.0", 'SERVICE': "WCS", 'REQUEST': "GetCapabilities"}
         response = self.query_server(params_73)
